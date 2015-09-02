d8ebc014c8e6898d419eca95e201180189da28ee
Sandilya Jandhyala <jysandilya@gmail.com>
2015-09-02 21:26:17 +0530

    Minor fix

--- chestify/security.py
+++ chestify/security.py
@@ -1,5 +1,5 @@
 def principal_callback(userid, request):
-	if userid == request.matchdict.get('user-id', ''):
+	if userid == request.matchdict.get('user_id', ''):
 		return ['principal:permitted']
 	else:
 		return []

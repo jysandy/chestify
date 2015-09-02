ec0ca541c7beea68be4b18f552bccc51d9cf600a
bhaskaramVaransi <varanasi.venkat1@gmail.com>
2015-09-02 20:04:29 +0530

    Added client side code

--- chestify/views.py
+++ chestify/views.py
@@ -2,7 +2,7 @@ from pyramid.view import view_config
 
 
 #TODO: Delete this later and replace it with our frontend
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
+@view_config(route_name='home', renderer='templates/index.pt')
 def my_view(request):
     return {'project': 'chestify'}
 

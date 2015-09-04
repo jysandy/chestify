081e298069c6d9e1fa5d82bc6c6644eca4a10de5
Sandilya Jandhyala <jysandilya@gmail.com>
2015-09-04 16:27:23 +0530

    Configured database connection

--- chestify/__init__.py
+++ chestify/__init__.py
@@ -1,6 +1,9 @@
+import os
+
 from pyramid.authentication import AuthTktAuthenticationPolicy
 from pyramid.authorization import ACLAuthorizationPolicy
 from pyramid.config import Configurator
+import sqlalchemy
 
 from .security import principal_callback
 
@@ -13,9 +16,25 @@ def main(global_config, **settings):
     config = Configurator(settings=settings, root_factory='.resources.Root')
     config.include('pyramid_chameleon')
 
+    #TODO: Create the SQLALchemy engine here
+    username = os.environ.get('CHESTIFY_DB_USER')
+    password = os.environ.get('CHESTIFY_DB_PASSWORD')
+    host = os.environ.get('OPENSHIFT_POSTGRESQL_DB_HOST')
+    port = os.environ.get('OPENSHIFT_POSTGRESQL_PORT')
+    database = 'chestify'
+    connect_url = sqlalchemy.engine.url.URL(username=username, password=password,
+                    host=host, port=port, database=database, drivername='psycopg2')
+    if host is not None:
+        #If we are on Openshift
+        engine = sqlalchemy.create_engine(connect_url)
+        DBSession.configure(bind=engine)
+        Base.metadata.bind = engine
+        Base.metadata.create_all()
+
+    secret_key = os.environ.get('CHESTIFY_SECRET_KEY', settings['chestify.secret'])
 	# Security policies
     authn_policy = AuthTktAuthenticationPolicy(
-        DEVEL_SECRET_KEY, callback=principal_callback,
+        secret_key, callback=principal_callback,
         hashalg='sha512')
     authz_policy = ACLAuthorizationPolicy()
     config.set_authentication_policy(authn_policy)

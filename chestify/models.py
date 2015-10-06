5f7ea222cd2f7f16bacf8bdf1203e3c4989de7dc
Sandilya Jandhyala <jysandilya@gmail.com>
2015-10-06 20:54:06 +0530

    Formatting changes

--- chestify/models.py
+++ chestify/models.py
@@ -14,9 +14,7 @@ from sqlalchemy.orm import (
 from zope.sqlalchemy import ZopeTransactionExtension
 
 
-
-DBSession = scoped_session(
-    sessionmaker(extension=ZopeTransactionExtension()))
+DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
 

651fd6db78692f7752e6e7694aa427846fe4fbdb
Sandilya Jandhyala <jysandilya@gmail.com>
2015-10-13 21:56:43 +0530

    Small fix

--- chestify/utils.py
+++ chestify/utils.py
@@ -4,7 +4,7 @@ from pyramid.httpexceptions import HTTPForbidden
 def require_login(view_func):
     def login_check(context, request):
         if request.authenticated_user is not None:
-            return view_func(request)
+            return view_func(context, request)
         else:
             return HTTPForbidden()
     return login_check
